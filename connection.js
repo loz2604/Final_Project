b6add7dbfadcc9a2a192f0daaa3aa7d3492cd12a
Lorraine <lorrainewilliams2604@gmail.com>
2021-11-01 14:20:15 +0000

    fixed port

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 if (process.env.NODE_ENV === "production") {
     module.exports = new Sequelize(`${process.env.DATABASE_URL}?sslmode=require`, {
         url: process.env.DATABASE_URL,
-        dialect: "postgres",
+        dialect: process.env.SQL_DB_DIALECT,
         dialectOptions: {
             ssl: {
                 rejectUnauthorized: false,
