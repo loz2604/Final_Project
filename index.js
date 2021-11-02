d5c2b1c9933b5a6adf87bfe0961d3efc271efeab
Lorraine <lorrainewilliams2604@gmail.com>
2021-11-02 10:29:36 +0000

    userLogin

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ const express = require("express");
 const passport = require("passport");
 const app = express();
 const cors = require("cors");
+
 const connection = require("./connection");
-// const { registerStrategy, loginStrategy, verifyStrategy } = require("./middleware/auth");
+const { registerStrategy, loginStrategy, verifyStrategy } = require("./middleware/auth");
 
-
+const User = require("./models/user");
+const userRouter = require("./routes/user");
 
 app.use(express.json());
 app.use(cors());
 app.use(passport.initialize());
 
-app.listen(process.env.PORT || 80, () => {
+passport.use("register", registerStrategy);
+passport.use("login", loginStrategy);
+passport.use("logout", verifyStrategy);
+
+app.use("/user", userRouter)
+
+app.listen(process.env.PORT, async () => {
     connection.authenticate();
+    await User.sync({ alter: true });
     console.log("App online");
 });
 
