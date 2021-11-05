f00a5018f62ded14e22e115b65a4a212b30ab5bb
Lorraine <lorrainewilliams2604@gmail.com>
2021-11-05 10:04:51 +0000

    add order function

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const connection = require("./connection");
 const { registerStrategy, loginStrategy, verifyStrategy } = require("./middleware/auth");
 
 const User = require("./models/user");
-
+const { Orders, OrderInfo } = require("./models/order");
 const userRouter = require("./routes/user");
+const orderRouter = require("./routes/order");
 const registerError = require("./middleware/errors");
 
 app.use(express.json());
@@ -18,10 +19,9 @@ app.use(cors());
 app.use(passport.initialize());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    console.log("got here")
-    next();
-  });
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 passport.use("register", registerStrategy);
 passport.use("login", loginStrategy);
@@ -29,16 +29,15 @@ passport.use(verifyStrategy);
 
 app.use("/test", (req, res) => {
   res.send("Hello World!")
-})
+});
 app.use("/user", userRouter);
+app.use("/orders", orderRouter);
 app.use(registerError);
 
 app.listen(process.env.PORT, async () => {
   connection.authenticate();
   await User.sync({ alter: true });
+  await Orders.sync({ alter: true });
+  await OrderInfo.sync({ alter: true });
   console.log("App online");
 });
-
-
-
-
