0eb5f16dc134f858531c150d115664796f1eb9d9
Lucas Lederman <ljlederman@gmail.com>
2021-11-10 11:38:02 +0000

    User history working

--- models/order.js
+++ models/order.js
@@ -4,19 +4,20 @@ const User = require("./user");
 const { Products } = require("./products");
 const { Basket } = require("./savedbasket");
 
-const Orders = connection.define("Order", {
-}
-);
+const Orders = connection.define("Order", {});
 
-const OrderInfo = connection.define("OrderInfo", { 
-}
-);
+const OrderInfo = connection.define("OrderInfo", {
+  quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+});
 
 OrderInfo.belongsTo(Orders, { onDelete: "cascade" });
 Orders.belongsTo(User, { onDelete: "cascade" });
 OrderInfo.belongsTo(Products, { onDelete: "cascade" });
-Basket.belongsTo(Products, {through: "Basketcase"});
+Basket.belongsTo(Products, { through: "Basketcase" });
 Basket.belongsTo(User, { onDelete: "cascade" });
 Products.belongsToMany(Basket, { through: "Basketcase" });
 
-module.exports = { Orders, OrderInfo }
+module.exports = { Orders, OrderInfo };
